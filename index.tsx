dded8822f88d6423d1d29e2fe032d05b5d1026d5
oujeon <oujeon@gmail.com>
2025-07-15 14:39:59 +0900

    page

---
+++ index.tsx
@@ -0,0 +1,3 @@
+import Button_sample from "./userdefined_components/button_sample";
+
+export { Button_sample };
---
+++ userdefined_components/button_sample.tsx
@@ -0,0 +1,6 @@
+"use client";
+import Button from "@mui/material/Button";
+
+export default function Button_sample() {
+  return <Button variant="text">Hello World</Button>;
+}
